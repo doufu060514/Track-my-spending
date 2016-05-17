7c23156a74acd8474b276c197da08fd9b990a46b
william <william@williamdemacbook-pro.local>
2016-05-18 01:29:35 +1200

    Initial Commit

---
+++ Test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by william on 18/05/16.
+//  Copyright © 2016 WilliamLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test2
+//
+//  Created by william on 18/05/16.
+//  Copyright © 2016 WilliamLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
